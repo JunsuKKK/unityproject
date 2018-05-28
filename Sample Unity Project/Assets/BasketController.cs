bb36e1c198ef75554bfb02948c83ed69bea2521c
jaewook <dnrdldnrdl62@naver.com>
2018-05-28 16:18:47 +0900

    sample project insert

---
+++ Sample Unity Project/Assets/BasketController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasketController : MonoBehaviour {
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.RightArrow)){
+			this.transform.Translate (Vector3.right * 60.0f * Time.deltaTime);
+		}
+		if (Input.GetKeyDown(KeyCode.LeftArrow)){
+			this.transform.Translate (Vector3.left * 60.0f * Time.deltaTime);
+		}
+		if (Input.GetKeyDown(KeyCode.UpArrow)) {
+			this.transform.Translate (Vector3.forward * 60.0f * Time.deltaTime);
+		}
+		if (Input.GetKeyDown(KeyCode.DownArrow)) {
+			this.transform.Translate (Vector3.back * 60.0f * Time.deltaTime);
+		}
+	}
+}
+
